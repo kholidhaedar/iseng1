5761ad52f0cdfd29a804ada77fd26f72729877c7
W. Kholid Haedar <kholidkhalid3@gmail.com>
2019-07-22 12:59:51 +0700

    Add files via upload

---
+++ triangle_char.py
@@ -0,0 +1,6 @@
+char = '#'
+count = 1
+masuk = int(input('berapa :'))
+for kali in range(masuk):
+    print(char*count)
+    count += 1
---
+++ triangle_char_many-0.py
@@ -0,0 +1,5 @@
+char = '#'
+masuk = int(input('berapa :'))
+for kali in range(masuk):
+    print(char*masuk)
+    masuk -= 1
